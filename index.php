dd2cbb551923223b7f4a2f47b0dfbbd82d82a0f6
Pyae Phyo Hein <pyaephyohein.info.3326@gmail.com>
2019-12-18 21:40:43 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,93 @@
-<!DOCTYPE HTML>
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-    p,h1 {
-        text-align: center;
-        font-size: 60px;
-        margin-top: 100px;
-    }
-    body {
-        background-color: gray;
-    }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
-
+<style>
+    @import url(https://fonts.googleapis.com/css?family=Syncopate);
+    /* p {
+        display: inline-block;
+        font-size: 80px;
+        line-height: 0.9;
+        padding: 10px;
+        font-family: 'Syncopate', sans-serif;
+        text-transform: uppercase;
+        background-color: white;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+    } */
+    p,h1 {
+        display: inline-block;
+        font-size: 80px;
+        line-height: 0.9;
+        padding: 20px;
+        font-family: 'Syncopate', sans-serif;
+        text-transform: uppercase;
+        background: radial-gradient(
+    circle farthest-corner at left center,
+    white,
+    #111
+  ) no-repeat;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+    }
+    div.border {
+        border: 3px solid #072144;
+        border-radius: 12px;
+        margin: 1px;
+        background: radial-gradient(circle farthest-corner at ,white,#111
+  ) no-repeat;
+    }
+body {
+    background: black;
+    text-align: center;
+    padding: 120px 20px;
+    }
+</style>
 <body>
-    <p id="demo"></p>
-    <script>
-   
-    var countDownDate = <?php echo strtotime('Jan 1, 2020 00:00:00') ?> * 1000;
-    var now = <?php echo time() ?> * 1000;
+</div>   
+    <div class="border">
+        <h1> New Year Countdown</h1>
+    </div>
+    <p>
+    <div class="border">
+            <!-- Display the countdown timer in an element -->
 
-    var x = setInterval(function() {
+        <p id="demo"></p>
 
+        <script>
+        // Set the date we're counting down to
+        var countDownDate = new Date("Jan 1, 2020 UTC+06:30").getTime();
 
+        // Update the count down every 1 second
+        var x = setInterval(function() {
 
+        // Get today's date and time
+        var now = new Date().getTime();
 
-        now = now + 1000;
-
-
+        // Find the distance between now and the count down date
         var distance = countDownDate - now;
 
-
+        // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = days + "D " + hours + "H "
+        + minutes + "M " + seconds + "s ";
 
-        document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-            minutes + "m " + seconds + "s ";
-
-
+        // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
             document.getElementById("demo").innerHTML = "EXPIRED";
         }
-    }, 1000);
-    </script>
-
-    <H1>NEW YEAR COUNTDOWN</H1>
+        }, 1000);
+        </script>
 </body>
-
 </html>
